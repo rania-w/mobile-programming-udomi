bb82bbbf004aae992ffc3799e6a4606f6e6c42eb
rania-w <rania.weiss@stu.ibu.edu.ba>
2022-06-10 21:37:45 +0200

    notifications for registration work - puppy sound doesn't ;(

--- app/src/main/java/com/example/udomiba/RegisterPage.java
+++ app/src/main/java/com/example/udomiba/RegisterPage.java
@@ -1,8 +1,17 @@
 package com.example.udomiba;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -12,6 +21,9 @@ public class RegisterPage extends AppCompatActivity {
 
     EditText email, phoneNumber, password, password2;
     public static final String EXTRA_ID="";
+    public static final String CHANNEL_ID="";
+    //Uri sound = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + RegisterPage.this.getPackageName() + "/" + R.raw.puppybarking);
+    //AudioAttributes attributes;
 
 
     @Override
@@ -23,6 +35,14 @@ public class RegisterPage extends AppCompatActivity {
         phoneNumber=findViewById(R.id.register_phone);
         password=findViewById(R.id.register_password);
         password2=findViewById(R.id.register_password2);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, "šanel", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+            //attributes = new AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_NOTIFICATION).build();
+        }
     }
 
     public void onRegisterButton(View view){
@@ -35,6 +55,15 @@ public class RegisterPage extends AppCompatActivity {
             Intent intent = new Intent(RegisterPage.this, LoginPage.class);
             intent.putExtra(EXTRA_ID, newId);
             startActivity(intent);
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(RegisterPage.this, CHANNEL_ID);
+            builder.setContentTitle("Udomi")
+                    .setContentText("Registracija uspješna")
+                    .setSmallIcon(R.drawable.dog)
+                    //.setSound(sound)
+            /*.addAction(neki drawable neshto, "šta se radi", intent)*/
+            ;
+            NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(RegisterPage.this);
+            notificationManagerCompat.notify(1, builder.build());
             Toast.makeText(this, "Registracija uspješna", Toast.LENGTH_SHORT).show();
         } else{
             Toast.makeText(this, "Email adresa zauzeta", Toast.LENGTH_SHORT).show();
